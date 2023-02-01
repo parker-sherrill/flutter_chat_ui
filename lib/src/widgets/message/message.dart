9922cd423dfe10e57bfd3baf6ed9ba335c199f7a
parker-sherrill <parker@circlapp.io>
2023-02-01 09:45:42 -0500

    fff

--- lib/src/widgets/message/message.dart
+++ lib/src/widgets/message/message.dart
@@ -297,7 +297,7 @@ class Message extends StatelessWidget {
                               text: TextSpan(
                                   children: <TextSpan> [
                                     const TextSpan(
-                                        text: ' ❤️ ', // emoji characters
+                                        text: '  ❤️ ', // emoji characters
                                       style: TextStyle(
                                       fontSize: 14,
                                       fontFamily: 'EmojiOne',
